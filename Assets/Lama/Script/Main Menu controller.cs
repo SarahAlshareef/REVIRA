8ea4128365bebef59441ef4aa0ad2587c56250b9
Lama-03 <Lama-4321-@hotmail.com>
2025-02-10 19:34:44 +0300

    Editing ( MainMenu-LoginScene-SignupScene )
    All edit on scenes

---
+++ Assets/Lama/Script/Main Menu controller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public Button signupButton;
+    public Button loginButton;
+
+    void Start()
+    {
+       
+
+        // Assign button click events
+        signupButton.onClick.AddListener(OpenSignup);
+        loginButton.onClick.AddListener(OpenLogin);
+    }
+
+
+    public void OpenSignup()
+    {
+        SceneManager.LoadScene("SignupScene"); // Ensure a scene named "SignupScene" exists
+    }
+
+    public void OpenLogin()
+    {
+        SceneManager.LoadScene("LoginScene"); // Ensure a scene named "LoginScene" exists
+    }
+}
