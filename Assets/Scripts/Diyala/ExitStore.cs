d798359366f7b433bd832e869980989597a989bd
Diyala Alturki <diyala.alturki@icloud.com>
2025-03-20 06:13:30 +0300

    Exit Store Script
    the pop up will appear in front of the user's camera with a distance of 2 meters

---
+++ Assets/Scripts/Diyala/ExitStore.cs
@@ -0,0 +1,45 @@
+// Unity
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+// C#
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExitStore : MonoBehaviour
+{
+    public GameObject popupPanel;
+    public Button confirmButton, cancelButton;
+    public Transform playerCamera;
+    public float popupDistance = 2.0f;
+
+    void Start()
+    {
+        popupPanel?.SetActive(false);
+
+        confirmButton?.onClick.AddListener(ConfirmExit);
+        cancelButton?.onClick.AddListener(ClosePopup);
+    }
+
+    public void ShowExitPopup()
+    {
+        if ( popupPanel != null && playerCamera != null )
+        {
+            popupPanel.transform.position = playerCamera.position + playerCamera.forward * popupDistance;
+            popupPanel.transform.LookAt(playerCamera);
+            popupPanel.transform.Rotate(0, 180, 0);
+            popupPanel.SetActive(true);
+        }
+    }
+
+    public void ConfirmExit()
+    {
+        popupPanel?.SetActive(false);
+        SceneManager.LoadScene("StoreSelection");
+    }
+
+    public void ClosePopup()
+    {
+        popupPanel?.SetActive(false);
+    }
+}
