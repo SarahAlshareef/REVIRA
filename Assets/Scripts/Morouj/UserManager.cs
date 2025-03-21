b0f7d61b750b7988838fbf6e2fd14058f8ecf398
Sarah Yousef Alshareef <sra2015ps@gmail.com>
2025-03-22 02:46:20 +0300

    Update UserManager.cs

--- Assets/Scripts/Morouj/UserManager.cs
+++ Assets/Scripts/Morouj/UserManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UserManager : MonoBehaviour
@@ -9,6 +10,10 @@ public class UserManager : MonoBehaviour
     public string LastName { get; private set; }
     public string Email { get; private set; }
     public float AccountBalance { get; private set; }
+
+    public Dictionary<string, Address> AddressBook { get; private set; } = new Dictionary<string, Address>();
+    public Address SelectedAddress { get; private set; }
+
 
     void Awake()
     {
@@ -29,5 +34,15 @@ public class UserManager : MonoBehaviour
         LastName = lastName;
         Email = email;
         AccountBalance = accountBalance;
+    }
+
+    public void SetAddressBook(Dictionary<string, Address> addressBook)
+    {
+        AddressBook = addressBook;
+    }
+
+    public void SetSelectedAddress(Address selectedAddress)
+    {
+        SelectedAddress = selectedAddress;
     }
 }
