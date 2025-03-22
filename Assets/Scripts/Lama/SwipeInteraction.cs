c7cda13547e51f8446df7deb12ab030ef61702cb
Asayl34 <asaylalmelabi@hotmail.com>
2025-03-22 07:02:58 +0300

    Scroll in Controllers

--- Assets/Scripts/Lama/SwipeInteraction.cs
+++ Assets/Scripts/Lama/SwipeInteraction.cs
@@ -5,28 +5,70 @@ using UnityEngine.UI;
 
 public class SwipInteraction : MonoBehaviour
 {
-    // 
+    
     public Scrollbar scrollbar;
     float scroll_pos = 0;
     float[] pos;
+    int childCount;
+    float distance;
+    public float controllerScrollSpeed = 2.5f;
+
+    void Start()
+    {
+        InitPositions();
+    }
+
 
     void Update()
     {
-        int childCount = transform.childCount;
-        pos = new float[childCount];
-        float distance = 1f / (childCount - 1f);
+        scroll_pos = scrollbar.value;
 
-        //
+        HandleMouseInput();
+        HandleControllerInput();
+        SnapToNearest();
+        ScaleChildren();
+    }
+    void InitPositions()
+    {
+
+        childCount = transform.childCount;
+        pos = new float[childCount];
+        distance = 1f / (childCount - 1f);
+
+        
         for (int i = 0; i < childCount; i++)
         {
             pos[i] = distance * i;
         }
+    }
 
-        // Always update scroll_pos from the scrollbar's current value.
-        scroll_pos = scrollbar.value;
 
-      
-        if (!Input.GetMouseButton(0))
+    void HandleMouseInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            scroll_pos = scrollbar.value;
+        }
+    }
+
+
+    void HandleControllerInput()
+        {
+            Vector2 thumbstick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+
+            if (Mathf.Abs(thumbstick.x) > 0.1f)
+            {
+                scroll_pos += thumbstick.x * controllerScrollSpeed * Time.deltaTime;
+                scroll_pos = Mathf.Clamp01(scroll_pos);
+                scrollbar.value = scroll_pos;
+            }
+        }
+
+    void SnapToNearest()
+    {
+        Vector2 thumbstick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+
+        if (!Input.GetMouseButton(0) || Mathf.Abs(thumbstick.x) < 0.1f)
         {
             for (int i = 0; i < childCount; i++)
             {
@@ -38,13 +80,15 @@ public class SwipInteraction : MonoBehaviour
                 }
             }
         }
+    }
 
-        // 
+    void ScaleChildren()
+    { 
         for (int i = 0; i < childCount; i++)
         {
             if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
             {
-                // 
+                
                 transform.GetChild(i).localScale = Vector3.Lerp(transform.GetChild(i).localScale, new Vector3(1f, 1f, 1f), 0.1f);
 
                 // Scale down all other children.
