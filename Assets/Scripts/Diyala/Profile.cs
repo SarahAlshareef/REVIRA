88eb5bc0e70b60de8bc0c5544b3d78b1c2e4a0d3
Diyala Alturki <diyala.alturki@icloud.com>
2025-04-12 13:30:44 +0300

    Update Profile.cs

--- Assets/Scripts/Diyala/Profile.cs
+++ Assets/Scripts/Diyala/Profile.cs
@@ -8,24 +8,18 @@ public class Profile : MonoBehaviour
     public Button openProfileButton;
     public Button closeProfileButton;
 
-  
     void Start()
     {
-        if (profilePanel != null)
-            profilePanel.SetActive(false);
-
+        profilePanel?.SetActive(false);
         openProfileButton?.onClick.AddListener(ShowProfile);
         closeProfileButton?.onClick.AddListener(HideProfile);
     }
-
     void ShowProfile()
     {
         profilePanel.SetActive(true);
     }
-
     void HideProfile()
     {
         profilePanel.SetActive(false);
     }
 }
-
