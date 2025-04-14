c1ff06ee278ff75ae262083c6454846a6036e703
Diyala Alturki <diyala.alturki@icloud.com>
2025-04-14 15:04:37 +0300

    Profile - 4

--- Assets/Scripts/Diyala/SceneTracker.cs
+++ Assets/Scripts/Diyala/SceneTracker.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class SceneTracker : MonoBehaviour
 {
     public static SceneTracker Instance { get; private set; }
-
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public string PreviousSceneName { get; private set; }
 
     public void SetPreviousScene(string SceneName)
